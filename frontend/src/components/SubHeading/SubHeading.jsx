2dd644bf8b828d26f05009544bb8e44645199513
aleksandersousa <aleksander-ssousa@outlook.com>
2022-02-08 23:35:57 -0300

    created header

--- frontend/src/components/SubHeading/SubHeading.jsx
+++ frontend/src/components/SubHeading/SubHeading.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import { images } from '../../constants';
 
-function SubHeading() {
-  return <div>SubHeading</div>;
+export default function SubHeading({ title }) {
+  return (
+    <div style={{ marginBottom: '2rem' }}>
+      <p className="p__cormorant">{title}</p>
+      <img src={images.spoon} alt="spoon" className="spoon__img" />
+    </div>
+  );
 }
-
-export default SubHeading;
